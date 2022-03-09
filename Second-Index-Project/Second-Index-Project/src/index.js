ca1795ff8891581e37dc775e030fb267ac8e7f91
Stav Naftali Sananes <77387159+Stav-Sananes@users.noreply.github.com>
2022-03-09 13:30:30 +0200

    Add files via upload
    The second project of reactJS

---
+++ Second-Index-Project/Second-Index-Project/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+import AuthContext from './store/auth-context';
+
+ReactDOM.render(<AuthContext.Provider><App /></AuthContext.Provider>, document.getElementById('root'));
