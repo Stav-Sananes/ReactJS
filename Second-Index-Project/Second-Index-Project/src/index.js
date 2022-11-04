536f9e087378c60bd0f34f2f70cdaa618a5a50c4
Stav Naftali Sananes <77387159+Stav-Sananes@users.noreply.github.com>
2022-11-04 11:28:08 +0200

    Delete Second-Index-Project directory

--- Second-Index-Project/Second-Index-Project/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-import AuthContext from './store/auth-context';
-
-ReactDOM.render(<AuthContext.Provider><App /></AuthContext.Provider>, document.getElementById('root'));
